f0a1c1b4b801327235e60a19740afe8eb2f9dd87
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 16:58:20 +0300

    updated: toggle button, switch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlintogglebuttonswitch"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlintogglebuttonswitch"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlintogglebuttonswitch/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlintogglebuttonswitch/MainActivity.kt
@@ -1,13 +1,43 @@
 package com.ismailmesutmujde.kotlintogglebuttonswitch
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlintogglebuttonswitch.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.switch1.setOnCheckedChangeListener{ compoundButton, b->
+            if (b) {
+                Log.e("Switch", "ON")
+            } else {
+                Log.e("Switch", "OFF")
+            }
+        }
+
+        bindingMain.toggleButton.setOnCheckedChangeListener{ compoundButton, b->
+            if (b) {
+                Log.e("Toggle Button", "ON")
+            } else {
+                Log.e("Toggle Button", "OFF")
+            }
+        }
+
+        bindingMain.button.setOnClickListener {
+            val switchStatus = bindingMain.switch1.isChecked
+            val toggleButtonStatus = bindingMain.toggleButton.isChecked
+
+            Log.e("Switch Status", switchStatus.toString())
+            Log.e("Toggle Button Status", toggleButtonStatus.toString())
+        }
 
     }
 }
